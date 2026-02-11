7bbfc5da87299a7ea0049aa42f9e786c13065b52
Akashv4554 <akashv1242005@gmail.com>
2026-02-11 16:27:34 +0530

    feat: add public RTI list and detail views

---
+++ backend/config/rti/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.rti_list, name='rti_list'),
+    path('<int:pk>/', views.rti_detail, name='rti_detail'),
+]
--- backend/config/rti/views.py
+++ backend/config/rti/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import RTIRequest
 
-# Create your views here.
+
+def rti_list(request):
+    rti_requests = RTIRequest.objects.all()
+    return render(request, 'rti/rti_list.html', {'rti_requests': rti_requests})
+
+
+def rti_detail(request, pk):
+    rti = get_object_or_404(RTIRequest, pk=pk)
+    return render(request, 'rti/rti_detail.html', {'rti': rti})
